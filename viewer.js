1069ffa33657beb8d4d1a38fec37bd5c7f0fe3f7
ApostlesClothing <eggninja36@gmail.com>
2025-06-22 13:08:35 -0500

    updated

--- viewer.js
+++ viewer.js
@@ -8,7 +8,7 @@ const params = new URLSearchParams(window.location.search);
 const category = params.get("category") || "hope";
 
 document.getElementById("daily-photo").src = `images/${category}/${imageNumber}.png`;
+
 document.getElementById("logo-hotspot").addEventListener("click", () => {
   window.location.href = "index.html";
 });
-
