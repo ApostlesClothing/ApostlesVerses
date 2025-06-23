69f77607c83469ec8f4ed67040eceb3be94bdd1f
ApostlesClothing <eggninja36@gmail.com>
2025-06-22 21:42:48 -0500

    hotspot

--- viewer.js
+++ viewer.js
@@ -5,9 +5,10 @@ const dayOfYear = Math.floor(diff / (1000 * 60 * 60 * 24));
 const imageNumber = (dayOfYear % 365) + 1;
 
 const params = new URLSearchParams(window.location.search);
-const category = params.get("category");
-const categoryEs = params.get("category-es");
+const category = params.get("category") || "hope";
 
-const folder = category || categoryEs || "hope";
+document.getElementById("daily-photo").src = `images/${category}/${imageNumber}.png`;
 
-document.getElementById("daily-photo").src = `images/${folder}/${imageNumber}.png`;
+document.getElementById("logo-hotspot").addEventListener("click", () => {
+  window.location.href = "index.html";
+});
